ecb7b65d6790e4cf8ef973ca0f78391c7c1f9923
momo_ai <momo_ai@noreply.gitea.io>
2021-12-28 10:56:51 +0800

    更新 'handler/models/user.go'

--- handler/models/user.go
+++ handler/models/user.go
@@ -3,10 +3,15 @@ package models
 import "time"
 
 type User struct {
+	Id       string     `json:"id" xorm:"id"`
 	Name       string    `json:"name" xorm:"name"`
 	JoinTime   time.Time `json:"join_time" xorm:"join_time"`
 	LoginIp    string    `json:"login_ip" xorm:"login_ip"`
-	Enable     string    `json:"enable" xorm:"enable"`
+	Status     string    `json:"status" xorm:"status"`
 	Dept       Dept      `json:"dept" xorm:"dept"`
 	Role       []*Role   `json:"role" xorm:"role"`
+}
+
+func(a *User) TableName() string {
+	return "user"
 }
