be1a9f285de43bda36eabeb7d8d602faa7edc508
momo_ai <momo_ai@noreply.gitea.io>
2021-12-28 10:57:59 +0800

    更新 'handler/views/user.go'

--- handler/views/user.go
+++ handler/views/user.go
@@ -1,6 +1,7 @@
 package views
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"sso/handler/services"
 	"sso/utils"
@@ -8,12 +9,12 @@ import (
 
 func QueryUserByNameHandler(c *gin.Context) {
 	// 获取参数
-	name := c.Param("name")
+	name := c.Query("name")
 	userService := services.GetService()
 	userDao, err := userService.Query("name",name)
-	if userDao == nil || err !=nil {
-		utils.BadResponse(c,utils.Error, "请求数据库错误")
+	if err !=nil {
+		utils.BadResponse(c,utils.Error, fmt.Sprintf("请求数据库错误:%v",err))
 		return
 	}
-	utils.NormalResponse(c, utils.OK, "",utils.ResponseObject{Data: userDao})
+	utils.NormalResponse(c, utils.OK, "success",userDao,len(userDao))
 }
