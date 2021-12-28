00a072b94c383056f52cbaefae38c779956852d2
momo_ai <momo_ai@noreply.gitea.io>
2021-12-28 10:54:36 +0800

    update tableName

--- handler/models/application.go
+++ handler/models/application.go
@@ -4,8 +4,11 @@ type Application struct {
 	Id       string  `json:"id" xorm:"id"`
 	Name     string  `json:"name" xorm:"name"`
 	Domain   string  `json:"domain" xorm:"domain"`
-	Enable   bool    `json:"enable" xorm:"enable"`
+	Status   bool    `json:"status" xorm:"status"`
 	Chargeman User   `json:"chargeman" xorm:"chargeman"`
 }
 
 
+func(a *Application) TableName() string {
+	return "application"
+}
