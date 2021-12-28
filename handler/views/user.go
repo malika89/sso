74fb3a60d1fa97402237beb108053341b77a588c
momo_ai <momo_ai@noreply.gitea.io>
2021-12-28 16:58:43 +0800

    更新 'handler/views/user.go'

--- handler/views/user.go
+++ handler/views/user.go
@@ -18,3 +18,48 @@ func QueryUserByNameHandler(c *gin.Context) {
 	}
 	utils.NormalResponse(c, utils.OK, "success",userDao,len(userDao))
 }
+
+func UpdateUserHandler(c *gin.Context) {
+	// 获取参数
+	userService := services.GetService()
+	if err :=c.BindJSON(&userService.Model);err !=nil{
+		utils.BadResponse(c,utils.Error,"传参错误")
+		return
+	}
+	err := userService.Update()
+	if err !=nil {
+		utils.BadResponse(c,utils.Error, fmt.Sprintf("修改失败:%v",err))
+		return
+	}
+	utils.NormalResponse(c, utils.OK, "修改成功",nil,0)
+}
+
+func AddUserHandler(c *gin.Context) {
+	// 获取参数
+	userService := services.GetService()
+	if err :=c.BindJSON(&userService.Model);err !=nil{
+		utils.BadResponse(c,utils.Error,"传参错误")
+		return
+	}
+	err := userService.Add()
+	if err !=nil {
+		utils.BadResponse(c,utils.Error, fmt.Sprintf("新增记录失败:%v",err))
+		return
+	}
+	utils.NormalResponse(c, utils.OK, "新增记录成功",nil,0)
+}
+
+func DeleteUserHandler(c *gin.Context) {
+	// 获取参数
+	userService := services.GetService()
+	if err :=c.BindJSON(&userService.Model);err !=nil{
+		utils.BadResponse(c,utils.Error,"传参错误")
+		return
+	}
+	err := userService.Delete()
+	if err !=nil {
+		utils.BadResponse(c,utils.Error, fmt.Sprintf("删除失败:%v",err))
+		return
+	}
+	utils.NormalResponse(c, utils.OK, "删除成功",nil,0)
+}
