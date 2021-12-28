41afecacb26e3768daef78c444f958399d506809
momo_ai <momo_ai@noreply.gitea.io>
2021-12-28 10:55:39 +0800

    更新 'handler/models/menu.go'

--- handler/models/menu.go
+++ handler/models/menu.go
@@ -4,8 +4,12 @@ package models
 type Menu struct {
 	Id           string     `json:"id" xorm:"id"`
 	Name         string     `json:"name" xorm:"name"`
-	Enable       bool       `json:"enable" xorm:"enable"`
+	Status       bool       `json:"status" xorm:"status"`
 	Category     string     `json:"category" xorm:"category"`  //page,button
 	Application  Application
 	Parent       *Menu      `json:"id" xorm:"id"`
+}
+
+func(a *Menu) TableName() string {
+	return "menu"
 }
