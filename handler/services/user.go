a4ef7debce5617fbe8a137497d0c17dc03eecf4f
momo_ai <momo_ai@noreply.gitea.io>
2021-12-28 16:57:47 +0800

    更新 'handler/services/user.go'

--- handler/services/user.go
+++ handler/services/user.go
@@ -14,3 +14,12 @@ func GetService() *service  {
 	serviceApp.Model = models.User{}
 	return &serviceApp
 }
+
+//Delete 支持批量删除
+func(s *service) Delete() error {
+	dbSession := s.GetSession()
+	if _, err := dbSession.Where(s.Model).Delete(&models.User{}); err != nil {
+		return err
+	}
+	return nil
+}
