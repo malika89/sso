0bbf855b88c56f95ccf035f8eed81ce8c52d1380
momo_ai <momo_ai@noreply.gitea.io>
2021-12-28 10:55:11 +0800

    更新 'handler/models/dept.go'

--- handler/models/dept.go
+++ handler/models/dept.go
@@ -4,18 +4,27 @@ type Organization struct {
 	Id      string   `json:"id" xorm:"id"`
 	Name    string   `json:"name" xorm:"name"`
 	Address string   `json:"address" xorm:"address"`
-	Enable  bool     `json:"enable" xorm:"enable"`
+	Status  bool     `json:"status" xorm:"status"`
 	Dept    []*Dept  `json:"dept" xorm:"dept"`
+}
+
+func(a *Organization) TableName() string {
+	return "organization"
 }
 
 type Dept struct {
 	Id          string   `json:"id" xorm:"id"`
 	Name        string   `json:"name" xorm:"name"`
 	Code        string   `json:"code" xorm:"code"`
-	Enable      bool     `json:"enable" xorm:"enable"`
+	Status      bool     `json:"status" xorm:"status"`
 	EmployeeNo  string   `json:"employee_no" xorm:"employee_no"`
 	SetUpTime   string   `json:"setup_time" xorm:"setup_time"`
-	Level       int      `json:"level" xorm:"level"`               //等级
+	Slevel       int     `json:"slevel" xorm:"slevel"`               //等级
 	Parent      *Dept    `json:"parent_id" xorm:"parent_id"`
 }
+
+func(a *Dept) TableName() string {
+	return "dept"
+}
+
 
