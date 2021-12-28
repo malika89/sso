00cf6d85485779f6b1da41bee491314301b15de2
momo_ai <momo_ai@noreply.gitea.io>
2021-12-28 10:56:28 +0800

    更新 'handler/models/role.go'

--- handler/models/role.go
+++ handler/models/role.go
@@ -2,8 +2,14 @@ package models
 
 
 type Role struct {
+	Id       string     `json:"id" xorm:"id"`
 	Name     string `json:"name" xorm:"name"`
 	IsSuper  bool   `json:"is_super" xorm:"is_super"`
-	Enable   bool   `json:"enable" xorm:"enable"`
-	Permt   []*Menu
+	Status   bool   `json:"status" xorm:"status"`
+	Permt    []*Menu
+}
+
+
+func(a *Role) TableName() string {
+	return "role"
 }
